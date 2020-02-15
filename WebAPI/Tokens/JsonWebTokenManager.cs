d2a60890b65e3e09d0aa38af1b935996edaa2ee0
Nick Gilbert <nick.gilbert@worldvision.org.uk>
2020-02-15 10:59:00 +0000

    Removed redundant code in JsonWebTokenManager and changed the secret value

--- WebAPI/Tokens/JsonWebTokenManager.cs
+++ WebAPI/Tokens/JsonWebTokenManager.cs
@@ -13,39 +13,9 @@ namespace WebAPI.Models
 {
     public class TokenManager
     {
-        // HMACSHA256 generated secret key required for encryption and decryption
-        // To do: store in the Key Vault for Azure implementation
-        private static string Secret = "XCAP05H6LoKvbRRa/QkqLNMI7cOHguaRyHzyg7n5qEkGjQmtBhz4SzYh4Fqwjyi3KJHlSXKPwVu2+bXr6CtpgQ==";
-
-        // Token genaration method where a token is created and a string version of it returned
-        public static string GenerateToken(string username)
-        {
-            /// <summary>
-            /// Token genaration method where a token is created and a string version of it returned.
-            /// Creates the descriptor object, which represents the main content of the JWT, 
-            /// such as the claims, the expiration date and the signing information. 
-            /// The token is created and a string version of it is returned. In this example
-            /// only the username claim is added but the list of claim types that can be added is 
-            /// large. 
-            /// It is also possible to add custom data to the token post creation by accessing the JWT
-            /// payload property with code like: token.Payload["NicksFavouriteFood"] = "Huel";
-            /// </summary>
-
-            byte[] key = Convert.FromBase64String(Secret);
-            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
-            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new[] {
-                      new Claim(ClaimTypes.Name, username)}),
-                Expires = DateTime.UtcNow.AddMinutes(60),
-                SigningCredentials = new SigningCredentials(securityKey,
-                SecurityAlgorithms.HmacSha256Signature)
-            };
-
-            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
-            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
-            return handler.WriteToken(token);
-        }
+        // key (secret) required for encryption and decryption. 
+        // Held in code for demo only. Store in the Key Vault for non-demo implementations
+        private static string Secret = "ALLWORKANDNOPLAYMAKESJACKADULLBOY123";
 
         // The method reads the token in string format and converts it into a JwtSecurityToken
         public static ClaimsPrincipal GetPrincipal(string token)
