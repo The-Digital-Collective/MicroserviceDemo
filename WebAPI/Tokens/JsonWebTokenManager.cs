0ff97fc5f508108bd63f4be34dc1c45204b938e8
Nick Gilbert <nick.gilbert@worldvision.org.uk>
2020-02-17 16:55:47 +0000

    Clean up comments in the token manager

--- WebAPI/Tokens/JsonWebTokenManager.cs
+++ WebAPI/Tokens/JsonWebTokenManager.cs
@@ -4,56 +4,14 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 /// <summary>
-/// This class creates and validates Jason Web Tokens (JWT). 
-/// It requires installation of the System.IdentityModel.Tokens.Jwt package. 
-/// The process uses symmetric encryption where a single key is used to encrypt 
-/// and decrypt information passed between applications. 
+/// This class validates the passe in Jason Web Tokens (JWT). To do this it decrypts token 
+/// using the secret, which is common between the calling and called applications (symmetric encryption). 
+/// To support JWT processing the application requires installation of the System.IdentityModel.Tokens.Jwt NuGet package. 
 /// </summary>
 namespace WebAPI.Models
 {
     public class TokenManager
     {
-        // key (secret) required for encryption and decryption. 
-        // Held in code for demo only. Store in the Key Vault for non-demo implementations
-        //private static string Secret = "ALLWORKANDNOPLAYMAKESJACKADULLBOY123";
-
-        // The method reads the token in string format and converts it into a JwtSecurityToken
-        public static ClaimsPrincipal GetPrincipal(string token, string secret)
-        {
-            try
-            {
-                // Convert the token (in string format) to a JWT Security Token
-                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
-
-                // Bonb out if null
-                if (jwtToken == null)
-                {
-                    return null;
-                }
-
-                // Generate parameters for use in the validation process and re-creates the key
-                // using the same secret as used in the token generation step
-                byte[] key = Convert.FromBase64String(secret);
-                TokenValidationParameters parameters = new TokenValidationParameters()
-                {
-                    RequireExpirationTime = true,
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    IssuerSigningKey = new SymmetricSecurityKey(key)
-                };
-
-                SecurityToken securityToken;
-                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
-
-                return principal;
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
-        }
-
         // This method creates the Principal object using the token and then extracts the Identity object out of it.
         public static string ValidateToken(string token, string secret)
         {
@@ -77,6 +35,44 @@ namespace WebAPI.Models
             Claim usernameClaim = identity.FindFirst(ClaimTypes.Name);
             username = usernameClaim.Value;
             return username;
+        }
+
+        // key (secret) required for encryption and decryption. 
+        // The method reads the token in string format and converts it into a JwtSecurityToken
+        public static ClaimsPrincipal GetPrincipal(string token, string secret)
+        {
+            try
+            {
+                // Convert the token (in string format) to a JWT Security Token
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                JwtSecurityToken jwtToken = (JwtSecurityToken)tokenHandler.ReadToken(token);
+
+                // Bomb out if null
+                if (jwtToken == null)
+                {
+                    return null;
+                }
+
+                // Generates parameters for use in the validation process and re-creates the key
+                // using the same secret as used in the token generation step
+                byte[] key = Convert.FromBase64String(secret);
+                TokenValidationParameters parameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+
+                SecurityToken securityToken;
+                ClaimsPrincipal principal = tokenHandler.ValidateToken(token, parameters, out securityToken);
+
+                return principal;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
     }
 
